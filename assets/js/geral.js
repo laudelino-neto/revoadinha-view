8e9c3ca16dd91069b6815baff5d3f174a4f24415
Kevin <albinodemetrok@gmail.com>
2022-07-24 10:35:01 -0300

    criando algumas telas muito bonitas

--- assets/js/geral.js
+++ assets/js/geral.js
@@ -1,14 +1,12 @@
 $(document).ready(()=>{
     setTimeout(() => {$("#logar").css("display", "flex"); $("#introduction").css('display', "none")}, 2500);
-
+    var srcImagemBanner;
     $("#btn-logar").on('click', () => {
-        $.ajax({
-            url: "http://10.10.221.66:8080/api/login/"+$("#cp-login").val()+":"+$("#cp-senha").val(),
-        }).done(function( data ) {
-            console.log(data);
-            $("#teste").text(data.bairro);
-        });
-        window.location.href = "file:///C:/Users/DEV-02/Desktop/revoadinha-frontend/page/listaDeRevoadas.html"
+        window.location.href = "file:./listaDeRevoadas.html"
+    });
+    
+    $(".evento").on('click', () => {
+        window.location.href = "file:./detalhesDoEvento.html";
     });
 });
 
